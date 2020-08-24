9ce6ea4c512327ac763926f40d8b63660e055f05
Lauren Chambers <lchambers@aclumorg>
2020-08-24 11:15:01 -0400

    fix footer issues

--- footer.php
+++ footer.php
@@ -1,23 +1,26 @@
 <!-- Footer for Police in Politics site -->
 <!doctype html>
 <div id="footer">
+
   <div align="center">
     <a href="https://mijente.net/" target="_blank">
-      <img src="img/mijente-logo.png" class="logo" height="30px">
+      <img src="img/mijente-logo.png" class="logo" style="max-height:30px; max-width:105px; width:20%;">
     </a>
     <a href="http://www.bu.edu/spark/" target="_blank">
-      <img src="img/spark-logo.png" class="logo" height="75px">
+      <img src="img/spark-logo.png" class="logo" style="max-height:75px; max-width:75px; width:20%;">
     </a>
     <a href="https://www.aclum.org/" target="_blank">
-      <img src="img/Logo_CMYK_Massachusetts_Massachusetts.png" class="logo" height="45px">
+      <img src="img/Logo_CMYK_Massachusetts_Massachusetts.png" class="logo" style="max-height:45px; max-width:86px; width:20%">
     </a>
     <a href="https://www.data.aclum.org/" target="_blank">
-      <img src="img/D4J-logo-transparent.png" class="logo" height="40px">
+      <img src="img/D4J-logo-transparent.png" class="logo" style="max-height:40px; max-width:111px;  width:20%;">
     </a>
   </div>
+
   <p align="center" style="opacity: 0.6;">
     Please contact
     <a href="mailto: police-in-politics@aclum.org">police-in-politics@aclum.org</a>
     with questions.
   </p>
+
 </div>
