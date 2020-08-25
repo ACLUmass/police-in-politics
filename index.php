67b39ca2b5255258abaaa7f5749ecd52f1857f05
Lauren Chambers <lchambers@aclumorg>
2020-08-25 18:20:13 -0400

    Update intro text

--- index.php
+++ index.php
@@ -3,54 +3,55 @@
 
 <html>
 
-    <?php include 'head.php';?>
+<?php include 'head.php';?>
 
-    <body>
+<body>
 
-        <?php include 'nav.php';?>
+  <?php include 'nav.php';?>
 
-        <div class="content">
+  <div class="content">
 
-            View plots in the different tabs to track total donations,
-           time series and top police donors to elected officials in
-           Boston and Massachusetts in the time period 2010-2020 (up
-           to and including the latest quarter filing).
-            <br>
-            <br>
-            <p align="center">
-              <em>
-                Read more about police donations to political campaigns on the
-                <a href="https://data.aclum.org/2020/08/10/police-in-politics-boston/">Data for Justice blog.</a>
-              </em>
-            </p>
+    <p>Police reform and budgeting has become a high priority in Boston, across the Commonwealth, and nationally. However, engagement by police staff and their unions in state and local politics often acts as a obstacle to meaningful reform.</p>
+    
+    <p>Click through the menu tabs to explore data on police-related donations to Mayor Walsh and to the Boston City Council from 2010 to 2020 (up to and including the most recent quarter filing).</p>
+    
 
-            <h3>About the Project</h3>
+    <p align="center">
+      <em>
+        Read more about police donations to political campaigns in Boston on the
+        <a href="https://data.aclum.org/2020/08/10/police-in-politics-boston/">Data for Justice blog.</a>
+      </em>
+    </p>
 
-            <p>
-              The Police in Politics project is a partnership between
-              <a href="https://mijente.net/">Mijente</a>
-              ,
-              <a href="http://www.bu.edu/spark/">BU Spark!</a>
-              , and the
-              <a href="https://www.aclum.org/">ACLU of Massachusetts</a>
-               to investigate police
-                       donations to Massachusetts politicians. It is supported by some great
-                       technical volunteers, including Steve Backman.
-            </p>
+    <h3>About the Project</h3>
 
-            <p>Please note that BU Spark! participates in this project as a
-                           technical partner, providing data processing and visualizations
-                          only, and that neither the lab nor the university necessarily
-                          endorses the political analysis of these data as interpreted
-                          by Mijente and ACLU-MA.</p>
+    <p>
+      The Police in Politics project is a partnership between
+      <a href="https://mijente.net/">Mijente</a>
+      ,
+      <a href="http://www.bu.edu/spark/">BU Spark!</a>
+      , and the
+      <a href="https://www.aclum.org/">ACLU of Massachusetts</a>
+      to investigate police
+      donations to Massachusetts politicians. It is supported by some great
+      technical volunteers, including Steve Backman.
+    </p>
 
-            <h3>About the Data</h3>
-            All data displayed here are compiled from publicly
-                         available information supplied by the
-            <a href="https://www.ocpf.us/">Massachusetts Office of Campaign and Political Finance (OCPF).</a>
-            
-        </div>
-    </body>
+    <p>Please note that BU Spark! participates in this project as a
+     technical partner, providing data processing and visualizations
+     only, and that neither the lab nor the university necessarily
+     endorses the political analysis of these data as interpreted
+   by Mijente and ACLU-MA.</p>
+
+   <p>This project is ongoing. Check back later for further analysis of police donations to elected officials across Massachusetts.</p>
+
+   <h3>About the Data</h3>
+   All data displayed here are compiled from publicly
+   available information supplied by the
+   <a href="https://www.ocpf.us/">Massachusetts Office of Campaign and Political Finance (OCPF).</a>
+   
+ </div>
+</body>
 
 </html>
 
