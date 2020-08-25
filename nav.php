1eb66230dc073188227ea8fa87311d9d238fa95b
Lauren Chambers <lchambers@aclumorg>
2020-08-25 18:20:00 -0400

    Add text hint to hamburger menu upon collapse

--- nav.php
+++ nav.php
@@ -2,74 +2,78 @@
 <!doctype html>
 
 <div id="title_panel">
-    <h1 style="margin-bottom: 0px; font-size:40px;"><a href="index.php">Police in Politics</a></h1>
-    <h4 style="margin-top: 0px;">Police Campaign Donations in Massachusetts, 2010-2020</h4>
-    <em>A project from Mijente, BU Spark!, and the ACLU of Massachusetts</em>
+  <h1 style="margin-bottom: 0px; font-size:40px;"><a href="index.php">Police in Politics</a></h1>
+  <h4 style="margin-top: 0px;">Police Campaign Donations in Massachusetts, 2010-2020</h4>
+  <em>A project from Mijente, BU Spark!, and the ACLU of Massachusetts</em>
+</div>
+
+<nav class="navbar navbar-default navbar-static-top navbar-expand-md navbar-dark" role="navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <!-- <span class="navbar-toggler-icon"><span>test</span></span> -->
+    <!-- Represents navbar collapsed menu on smaller screens -->
+    <span class="sr-only">Toggle navigation</span>
+    <i class="fa fa-bars" aria-hidden="true"></i> Menu
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="nav navbar-nav" id="panels" data-tabsetid="2360">
+
+      <li>
+        <a href="index.php">About</a>
+      </li>
+
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-value="Mayor Walsh">
+          Mayor Walsh
+          <b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu" data-tabsetid="2312">
+          <li>
+            <a href="walsh-donations-v-time.php">Police Donations over Time</a>
+          </li>
+          <li>
+            <a href="walsh-top-donors.php">Top Police Donors</a>
+          </li>
+          <li>
+            <a href="walsh-donor-map.php">Police Donations by Zip Code</a>
+          </li>
+          <li>
+            <a href="walsh-donation-recurrence.php">Recurrence and Scale of Police Donations</a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-value="Current Boston City Councilors">
+          Current Boston City Councilors
+          <b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu" data-tabsetid="9661">
+          <li>
+            <a href="current-bcc-total-donations.php">Total Police Donations</a>
+          </li>
+          <li>
+            <a href="current-bcc-share.php">Share of Police Donations</a>
+          </li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-value="Boston City Councilors 2010-2020">
+          Boston City Councilors 2010-2020
+          <b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu" data-tabsetid="4788">
+          <li>
+            <a href="bcc-2010-2020-total-donations.php">Total Police Donations</a>
+          </li>
+          <li>
+            <a href="bcc-2010-2020-donations-v-time.php">Police Donations Over Time</a>
+          </li>
+          <li>
+            <a href="bcc-2010-2020-share.php">Share of Police Donations</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
   </div>
-  <nav class="navbar navbar-default navbar-static-top navbar-expand-md navbar-dark" role="navigation">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="nav navbar-nav" id="panels" data-tabsetid="2360">
-
-        <li>
-          <a href="index.php">About</a>
-        </li>
-
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-value="Mayor Walsh">
-            Mayor Walsh
-            <b class="caret"></b>
-          </a>
-          <ul class="dropdown-menu" data-tabsetid="2312">
-            <li>
-              <a href="walsh-donations-v-time.php">Police Donations over Time</a>
-            </li>
-            <li>
-              <a href="walsh-top-donors.php">Top Police Donors</a>
-            </li>
-            <li>
-              <a href="walsh-donor-map.php">Police Donations by Zip Code</a>
-            </li>
-            <li>
-              <a href="walsh-donation-recurrence.php">Recurrence and Scale of Police Donations</a>
-            </li>
-          </ul>
-        </li>
-
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-value="Current Boston City Councilors">
-            Current Boston City Councilors
-            <b class="caret"></b>
-          </a>
-          <ul class="dropdown-menu" data-tabsetid="9661">
-            <li>
-              <a href="current-bcc-total-donations.php">Total Police Donations</a>
-            </li>
-            <li>
-              <a href="current-bcc-share.php">Share of Police Donations</a>
-            </li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-value="Boston City Councilors 2010-2020">
-            Boston City Councilors 2010-2020
-            <b class="caret"></b>
-          </a>
-          <ul class="dropdown-menu" data-tabsetid="4788">
-            <li>
-              <a href="bcc-2010-2020-total-donations.php">Total Police Donations</a>
-            </li>
-            <li>
-              <a href="bcc-2010-2020-donations-v-time.php">Police Donations Over Time</a>
-            </li>
-            <li>
-              <a href="bcc-2010-2020-share.php">Share of Police Donations</a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </nav>
+</nav>
